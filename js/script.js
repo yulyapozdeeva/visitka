9948f374f92322c767a330b4fd1f5dbf71ab2cbb
yulyapozdeeva <yulya.pozdeeva.99@list.ru>
2022-03-25 12:46:04 +0300

    fff

--- js/script.js
+++ js/script.js
@@ -1,292 +1,3 @@
-var isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
-if (isMobile.any()) { }
-
-if (location.hash) {
-	var hsh = location.hash.replace('#', '');
-	if ($('.popup-' + hsh).length > 0) {
-		popupOpen(hsh);
-	} else if ($('div.' + hsh).length > 0) {
-		$('body,html').animate({ scrollTop: $('div.' + hsh).offset().top, }, 500, function () { });
-	}
-}
-$('.wrapper').addClass('loaded');
-
-var act = "click";
-if (isMobile.iOS()) {
-	var act = "touchstart";
-}
-
-let iconMenu = document.querySelector(".icon-menu");
-let body = document.querySelector("body");
-let menuBody = document.querySelector(".menu__body");
-if (iconMenu) {
-	iconMenu.addEventListener("click", function () {
-		iconMenu.classList.toggle("active");
-		body.classList.toggle("lock");
-		menuBody.classList.toggle("active");
-	});
-}
-
-//ZOOM
-if ($('.gallery').length > 0) {
-	baguetteBox.run('.gallery', {
-		// Custom options
-	});
-}
-/*
-CLOUD-ZOOM
-<a rel="position:'right',adjustX:25,adjustY:0,Width: 432" href="img/product/zoom.jpg" class="cloud-zoom product-main-mainimage__item">
-	<img class="cloudzoom-gallery" src="img/product/zoom.jpg" alt="" />
-</a>
-*/
-
-
-//POPUP
-$('.pl').click(function (event) {
-	var pl = $(this).attr('href').replace('#', '');
-	var v = $(this).data('vid');
-	popupOpen(pl, v);
-	return false;
-});
-function popupOpen(pl, v) {
-	$('.popup').removeClass('active').hide();
-	if (!$('.menu__body').hasClass('active')) {
-		//$('body').data('scroll',$(window).scrollTop());
-	}
-	if (!isMobile.any()) {
-		$('body').css({ paddingRight: $(window).outerWidth() - $('.wrapper').outerWidth() }).addClass('lock');
-		$('.pdb').css({ paddingRight: $(window).outerWidth() - $('.wrapper').outerWidth() });
-	} else {
-		setTimeout(function () {
-			$('body').addClass('lock');
-		}, 300);
-	}
-	history.pushState('', '', '#' + pl);
-	if (v != '' && v != null) {
-		$('.popup-' + pl + ' .popup-video__value').html('<iframe src="https://www.youtube.com/embed/' + v + '?autoplay=1"  allow="autoplay; encrypted-media" allowfullscreen></iframe>');
-	}
-	$('.popup-' + pl).fadeIn(300).delay(300).addClass('active');
-
-	if ($('.popup-' + pl).find('.slick-slider').length > 0) {
-		$('.popup-' + pl).find('.slick-slider').slick('setPosition');
-	}
-}
-function openPopupById(popup_id) {
-	$('#' + popup_id).fadeIn(300).delay(300).addClass('active');
-}
-function popupClose() {
-	$('.popup').removeClass('active').fadeOut(300);
-	if (!$('.menu__body').hasClass('active')) {
-		if (!isMobile.any()) {
-			setTimeout(function () {
-				$('body').css({ paddingRight: 0 });
-				$('.pdb').css({ paddingRight: 0 });
-			}, 200);
-			setTimeout(function () {
-				$('body').removeClass('lock');
-				//$('body,html').scrollTop(parseInt($('body').data('scroll')));
-			}, 200);
-		} else {
-			$('body').removeClass('lock');
-			//$('body,html').scrollTop(parseInt($('body').data('scroll')));
-		}
-	}
-	$('.popup-video__value').html('');
-
-	history.pushState('', '', window.location.href.split('#')[0]);
-}
-$('.popup-close,.popup__close').click(function (event) {
-	popupClose();
-	return false;
-});
-$('.popup').click(function (e) {
-	if (!$(e.target).is(".popup>.popup-table>.cell *") || $(e.target).is(".popup-close") || $(e.target).is(".popup__close")) {
-		popupClose();
-		return false;
-	}
-});
-$(document).on('keydown', function (e) {
-	if (e.which == 27) {
-		popupClose();
-	}
-});
-
-$('.goto').click(function () {
-	var el = $(this).attr('href').replace('#', '');
-	var offset = 0;
-	$('body,html').animate({ scrollTop: $('.' + el).offset().top + offset }, 500, function () { });
-
-	if ($('.menu__body').hasClass('active')) {
-		$('.menu__body,.icon-menu').removeClass('active');
-		$('body').removeClass('lock');
-	}
-	return false;
-});
-
-
-// function ibg() {
-// 	if (isIE()) {
-// 		let ibg = document.querySelectorAll(".ibg");
-// 		for (var i = 0; i < ibg.length; i++) {
-// 			if (ibg[i].querySelector('img') && ibg[i].querySelector('img').getAttribute('src') != null) {
-// 				ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
-// 			}
-// 		}
-// 	}
-// }
-// ibg();
-
-
-//Клик вне области
-$(document).on('click touchstart', function (e) {
-	if (!$(e.target).is(".select *")) {
-		$('.select').removeClass('active');
-	};
-});
-
-//UP
-$(window).scroll(function () {
-	var w = $(window).width();
-	if ($(window).scrollTop() > 50) {
-		$('#up').fadeIn(300);
-	} else {
-		$('#up').fadeOut(300);
-	}
-});
-$('#up').click(function (event) {
-	$('body,html').animate({ scrollTop: 0 }, 300);
-});
-
-$('body').on('click', '.tab__navitem', function (event) {
-	var eq = $(this).index();
-	if ($(this).hasClass('parent')) {
-		var eq = $(this).parent().index();
-	}
-	if (!$(this).hasClass('active')) {
-		$(this).closest('.tabs').find('.tab__navitem').removeClass('active');
-		$(this).addClass('active');
-		$(this).closest('.tabs').find('.tab__item').removeClass('active').eq(eq).addClass('active');
-		if ($(this).closest('.tabs').find('.slick-slider').length > 0) {
-			$(this).closest('.tabs').find('.slick-slider').slick('setPosition');
-		}
-	}
-});
-$.each($('.spoller.active'), function (index, val) {
-	$(this).next().show();
-});
-$('body').on('click', '.spoller', function (event) {
-	if ($(this).hasClass('mob') && !isMobile.any()) {
-		return false;
-	}
-
-	if ($(this).parents('.one').length > 0) {
-		$(this).parents('.one').find('.spoller').not($(this)).removeClass('active').next().slideUp(300);
-		$(this).parents('.one').find('.spoller').not($(this)).parent().removeClass('active');
-	}
-
-	if ($(this).hasClass('closeall') && !$(this).hasClass('active')) {
-		$.each($(this).closest('.spollers').find('.spoller'), function (index, val) {
-			$(this).removeClass('active');
-			$(this).next().slideUp(300);
-		});
-	}
-	$(this).toggleClass('active').next().slideToggle(300, function (index, val) {
-		if ($(this).parent().find('.slick-slider').length > 0) {
-			$(this).parent().find('.slick-slider').slick('setPosition');
-		}
-	});
-	return false;
-});
-
-
-
-function scrolloptions() {
-	var scs = 100;
-	var mss = 50;
-	var bns = false;
-	if (isMobile.any()) {
-		scs = 10;
-		mss = 1;
-		bns = true;
-	}
-	var opt = {
-		cursorcolor: "#fff",
-		cursorwidth: "4px",
-		background: "",
-		autohidemode: true,
-		cursoropacitymax: 0.4,
-		bouncescroll: bns,
-		cursorborderradius: "0px",
-		scrollspeed: scs,
-		mousescrollstep: mss,
-		directionlockdeadzone: 0,
-		cursorborder: "0px solid #fff",
-	};
-	return opt;
-}
-function scroll() {
-	$('.scroll-body').niceScroll('.scroll-list', scrolloptions());
-}
-if (navigator.appVersion.indexOf("Mac") != -1) {
-} else {
-	if ($('.scroll-body').length > 0) { scroll(); }
-}
-
-/*
-function scrollwhouse(){
-		var scs=100;
-		var mss=50;
-		var bns=false;
-	if(isMobile.any()){
-		scs=10;
-		mss=1;
-		bns=true;
-	}
-	var opt={
-		cursorcolor:"#afafaf",
-		cursorwidth: "5px",
-		background: "",
-		autohidemode:false,
-		railalign: 'left',
-		cursoropacitymax: 1,
-		bouncescroll:bns,
-		cursorborderradius: "0px",
-		scrollspeed:scs,
-		mousescrollstep:mss,
-		directionlockdeadzone:0,
-		cursorborder: "0px solid #fff",
-	};
-	return opt;
-}
-$('.whouse-content-body').niceScroll('.whouse-content-scroll',scrollwhouse());
-$('.whouse-content-body').scroll(function(event) {
-		var s=$(this).scrollTop();
-		var r=Math.abs($(this).outerHeight()-$('.whouse-content-scroll').outerHeight());
-		var p=s/r*100;
-	$('.whouse-content__shadow').css({opacity:1-1/100*p});
-});
-*/
-
-
-if ($('.t,.tip').length > 0) {
-	tip();
-}
-function tip() {
-	$('.t,.tip').webuiPopover({
-		placement: 'top',
-		trigger: 'hover',
-		backdrop: false,
-		//selector:true,
-		animation: 'fade',
-		dismissible: true,
-		padding: false,
-		//hideEmpty: true
-		onShow: function ($element) { },
-		onHide: function ($element) { },
-	}).on('show.webui.popover hide.webui.popover', function (e) {
-		$(this).toggleClass('active');
-	});
-}
 
 let myImageSlider = new Swiper('.image-slider', {
 	// Стрелки
@@ -360,7 +71,7 @@ let myImageSlider = new Swiper('.image-slider', {
 	slidesPerGroup: 1,
 
 	// Активный слайд по центру
-	centeredSlides: false,
+	centeredSlides: true,
 
 	// Стартовый слайд.
 	initialSlide: 0,
@@ -393,41 +104,50 @@ let myImageSlider = new Swiper('.image-slider', {
 	breakpoints: {
 		320: {
 			slidesPerView: 1,
+			spaceBetween: 10,
 		},
 		480: {
 			slidesPerView: 1,
+			spaceBetween: 0,
 		},
 		992: {
-			slidesPerView: 1,
+			slidesPerView: 2,
 		},
-		1480: {
-			slidesPerView: 1,
+		1182: {
+			//spaceBetween: 0,
+		},
+		1334: {
+			//spaceBetween: 40,
+		},
+		1483: {
+			slidesPerView: 2,
 		}
 	},
 
 
 	// Брейк поинты (адаптив)
 	// Соотношение сторон
-	breakpoints: {
-		'@0.15': {
-			slidesPerView: 1,
-		},
-		'@0.49': {
-			slidesPerView: 1,
-		},
-		'@0.75': {
-			slidesPerView: 1,
-		},
-		'@0.955': {
-			slidesPerView: 1,
-		},
-		'@1.22': {
-			slidesPerView: 1,
-		},
-		'@1.40': {
-			slidesPerView: 2,
-		}
-	},
+	// breakpoints: {
+	// 	'@0.15': {
+	// 		slidesPerView: 1,
+	// 	},
+	// 	'@0.49': {
+	// 		slidesPerView: 1,
+	// 	},
+	// 	'@0.75': {
+	// 		slidesPerView: 1,
+	// 	},
+	// 	'@0.955': {
+	// 		slidesPerView: 1,
+	// 	},
+	// 	'@1.22': {
+	// 		slidesPerView: 1,
+	// 	},
+	// 	'@1.40': {
+	// 		slidesPerView: 2,
+	// 	}
+	// },
+
 
 
 	// Отключить предзагрузка картинок
@@ -469,3 +189,386 @@ let myImageSlider = new Swiper('.image-slider', {
 	// элементов слайда
 	observeSlideChildren: true,
 });
+
+let myImageSlider2 = new Swiper('.image-slider-2', {
+	// Стрелки
+	// Навигация 
+	// Буллеты, текущее положение, прогрессбар
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	// Включение/отключение
+	// перетаскивания на ПК
+	simulateTouch: true,
+	// Чувствительность свайпа
+	touchRatio: 1,
+	// Угол срабатывания свайпа/перетаскивания
+	touchAngle: 45,
+	// Курсор перетаскивания
+	grabCursor: true,
+
+	// Переключение при клике на слайд
+	slideToClickedSlide: false,
+
+	// Навигация по хешу
+	hashNavigation: {
+		// Отслеживать состояние
+		watchState: true,
+	},
+
+	// Управление клавиатурой
+	keyboard: {
+		// Включить\выключить
+		enabled: true,
+		// Включить\выключить
+		// только когда слайдер
+		// в пределах вьюпорта
+		onlyInViewport: true,
+		// Включить\выключить
+		// управление клавишами
+		// pageUp, pageDown
+		pageUpDown: true,
+	},
+
+	// Управление колесом мыши
+	mousewheel: {
+		// Чувствительность колеса мыши
+		sensitivity: 1,
+		// Класс объекта на котором
+		// будет срабатывать прокрутка мышью.
+		//eventsTarget: ".image-slider"
+	},
+
+	// Автовысота
+	autoHeight: false,
+
+	// Количество слайдов для показа
+	slidesPerView: 2,
+
+	// Отключение функционала
+	// если слайдов меньше чем нужно
+	watchOverflow: false,
+
+	// Отступ между слайдами
+	spaceBetween: 114,
+
+	// Количество пролистываемых слайдов
+	slidesPerGroup: 1,
+
+	// Активный слайд по центру
+	centeredSlides: true,
+
+	// Стартовый слайд.
+	initialSlide: 0,
+
+	// Мультирядность
+	slidesPerColumn: 1,
+
+	// Бесконечный слайдер
+	loop: true,
+
+	// Кол-во дублирующих слайдов
+	loopedSlides: 0,
+
+	// Свободный режим
+	freeMode: true,
+
+	// Скорость
+	speed: 800,
+
+	// Вертикальный слайдер
+	direction: 'horizontal',
+
+
+	// Эффекты переключения слайдов.
+	// Листание
+	effect: 'slide',
+
+	// Брейк поинты(адаптив)
+	// Ширина экрана
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 10,
+		},
+		480: {
+			slidesPerView: 1,
+			spaceBetween: 0,
+		},
+		992: {
+			slidesPerView: 2,
+		},
+		1182: {
+			//spaceBetween: 0,
+		},
+		1334: {
+			//spaceBetween: 40,
+		},
+		1483: {
+			slidesPerView: 2,
+		}
+	},
+
+
+	// Брейк поинты (адаптив)
+	// Соотношение сторон
+	// breakpoints: {
+	// 	'@0.15': {
+	// 		slidesPerView: 1,
+	// 	},
+	// 	'@0.49': {
+	// 		slidesPerView: 1,
+	// 	},
+	// 	'@0.75': {
+	// 		slidesPerView: 1,
+	// 	},
+	// 	'@0.955': {
+	// 		slidesPerView: 1,
+	// 	},
+	// 	'@1.22': {
+	// 		slidesPerView: 1,
+	// 	},
+	// 	'@1.40': {
+	// 		slidesPerView: 2,
+	// 	}
+	// },
+
+
+
+	// Отключить предзагрузка картинок
+	preloadImages: false,
+	// Lazy Loading
+	// (подгрузка картинок)
+	lazy: {
+		// Подгружать на старте
+		// переключения слайда
+		loadOnTransitionStart: false,
+		// Подгрузить предыдущую
+		// и следующую картинки
+		loadPrevNext: false,
+	},
+	// Слежка за видимыми слайдами
+	watchSlidesProgress: true,
+	// Добавление класса видимым слайдам
+	watchSlidesVisibility: true,
+
+	// Зум картинки
+	zoom: {
+		// Макимальное увеличение
+		maxRatio: 5,
+		// Минимальное увеличение
+		minRatio: 1,
+	},
+
+	// Обновить свайпер
+	// при изменении элементов слайдера
+	observer: true,
+
+	// Обновить свайпер
+	// при изменении родительских
+	// элементов слайдера
+	observeParents: true,
+
+	// Обновить свайпер
+	// при изменении дочерних
+	// элементов слайда
+	observeSlideChildren: true,
+});
+
+let myImageSlider3 = new Swiper('.image-slider-3', {
+	// Стрелки
+	// Навигация 
+	// Буллеты, текущее положение, прогрессбар
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	// Включение/отключение
+	// перетаскивания на ПК
+	simulateTouch: true,
+	// Чувствительность свайпа
+	touchRatio: 1,
+	// Угол срабатывания свайпа/перетаскивания
+	touchAngle: 45,
+	// Курсор перетаскивания
+	grabCursor: true,
+
+	// Переключение при клике на слайд
+	slideToClickedSlide: false,
+
+	// Навигация по хешу
+	hashNavigation: {
+		// Отслеживать состояние
+		watchState: true,
+	},
+
+	// Управление клавиатурой
+	keyboard: {
+		// Включить\выключить
+		enabled: true,
+		// Включить\выключить
+		// только когда слайдер
+		// в пределах вьюпорта
+		onlyInViewport: true,
+		// Включить\выключить
+		// управление клавишами
+		// pageUp, pageDown
+		pageUpDown: true,
+	},
+
+	// Управление колесом мыши
+	mousewheel: {
+		// Чувствительность колеса мыши
+		sensitivity: 1,
+		// Класс объекта на котором
+		// будет срабатывать прокрутка мышью.
+		//eventsTarget: ".image-slider"
+	},
+
+	// Автовысота
+	autoHeight: false,
+
+	// Количество слайдов для показа
+	slidesPerView: 2,
+
+	// Отключение функционала
+	// если слайдов меньше чем нужно
+	watchOverflow: false,
+
+	// Отступ между слайдами
+	spaceBetween: 114,
+
+	// Количество пролистываемых слайдов
+	slidesPerGroup: 1,
+
+	// Активный слайд по центру
+	centeredSlides: true,
+
+	// Стартовый слайд.
+	initialSlide: 0,
+
+	// Мультирядность
+	slidesPerColumn: 1,
+
+	// Бесконечный слайдер
+	loop: true,
+
+	// Кол-во дублирующих слайдов
+	loopedSlides: 0,
+
+	// Свободный режим
+	freeMode: true,
+
+	// Скорость
+	speed: 800,
+
+	// Вертикальный слайдер
+	direction: 'horizontal',
+
+
+	// Эффекты переключения слайдов.
+	// Листание
+	effect: 'slide',
+
+	// Брейк поинты(адаптив)
+	// Ширина экрана
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 10,
+		},
+		480: {
+			slidesPerView: 1,
+			spaceBetween: 0,
+		},
+		992: {
+			slidesPerView: 2,
+		},
+		1182: {
+			//spaceBetween: 0,
+		},
+		1334: {
+			//spaceBetween: 40,
+		},
+		1483: {
+			slidesPerView: 2,
+		}
+	},
+
+
+	// Брейк поинты (адаптив)
+	// Соотношение сторон
+	// breakpoints: {
+	// 	'@0.15': {
+	// 		slidesPerView: 1,
+	// 	},
+	// 	'@0.49': {
+	// 		slidesPerView: 1,
+	// 	},
+	// 	'@0.75': {
+	// 		slidesPerView: 1,
+	// 	},
+	// 	'@0.955': {
+	// 		slidesPerView: 1,
+	// 	},
+	// 	'@1.22': {
+	// 		slidesPerView: 1,
+	// 	},
+	// 	'@1.40': {
+	// 		slidesPerView: 2,
+	// 	}
+	// },
+
+
+
+	// Отключить предзагрузка картинок
+	preloadImages: false,
+	// Lazy Loading
+	// (подгрузка картинок)
+	lazy: {
+		// Подгружать на старте
+		// переключения слайда
+		loadOnTransitionStart: false,
+		// Подгрузить предыдущую
+		// и следующую картинки
+		loadPrevNext: false,
+	},
+	// Слежка за видимыми слайдами
+	watchSlidesProgress: true,
+	// Добавление класса видимым слайдам
+	watchSlidesVisibility: true,
+
+	// Зум картинки
+	zoom: {
+		// Макимальное увеличение
+		maxRatio: 5,
+		// Минимальное увеличение
+		minRatio: 1,
+	},
+
+	// Обновить свайпер
+	// при изменении элементов слайдера
+	observer: true,
+
+	// Обновить свайпер
+	// при изменении родительских
+	// элементов слайдера
+	observeParents: true,
+
+	// Обновить свайпер
+	// при изменении дочерних
+	// элементов слайда
+	observeSlideChildren: true,
+});
+
